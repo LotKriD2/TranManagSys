5e772bbf35b15f00646430459ae4623205a0a089
LotKriD2 <91753208+LotKriD2@users.noreply.github.com>
2021-11-15 19:34:16 +0300

    Добавлены unit-тесты

---
+++ TranManagSysUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TranManagSysUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TranManagSysUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0de36d85-98ab-4b34-b8f0-47d278f4daa4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TranManagSysUnitTestProject/UnitTest.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TranManagSysLibrary;
+
+namespace TranManagSysUnitTestProject
+{
+    [TestClass()]
+    public class UnitTest
+    {
+        [TestMethod()]
+        public void AddEntryTest()
+        {
+            string login = "Admin";
+            string password = "admin";
+            string city = "Тверь";
+            User.Athorization(login, password, city);
+            Assert.AreEqual(User.AuthorizationSuccessful, true);
+            try
+            {
+                string nameTable = "Города";
+                string condition = "( Код_города  , Название_города )";
+                string values = "( 100, 'TestCity' )";
+                condition = condition + " values " + values;
+                DBConnection.AddEntry(nameTable, condition);
+                Assert.AreEqual(0, 0);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+        [TestMethod()]
+        public void UpdateEntryTest()
+        {
+            string login = "Admin";
+            string password = "admin";
+            string city = "Тверь";
+            User.Athorization(login, password, city);
+            Assert.AreEqual(User.AuthorizationSuccessful, true);
+            try
+            {
+                string nameTable = "Города";
+                string condition = "Название_города = 'UpdatedTestCity'";
+                condition = "set " + condition + " where Код_города = 2";
+                DBConnection.UpdateEntry(nameTable, condition);
+                Assert.AreEqual(0, 0);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+        [TestMethod()]
+        public void DeleteEntryTest()
+        {
+            string login = "Admin";
+            string password = "admin";
+            string city = "Тверь";
+            User.Athorization(login, password, city);
+            Assert.AreEqual(User.AuthorizationSuccessful, true);
+            try
+            {
+                string nameTable = "Города";
+                string condition = "Код_города = 3";
+                DBConnection.DeleteEntry(nameTable, condition);
+                Assert.AreEqual(0, 0);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+    }
+}
